6fbf27932ae1b5b7ca332616bc7b6d69e96db40f
CheckerT08 <tom.tb.brand@gmx.de>
2025-05-08 16:09:17 +0200

    Add Platformer Unity Project

---
+++ Platformer/Assets/Player/Camera/CameraManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager instance;
+
+    [SerializeField] private CinemachineVirtualCamera[] allVirtualCameras;
+
+    [Header("Controls for lerping the Y Damping during player jump/fall")]
+    [SerializeField] private float fallPanAmount = 0.25f;
+    [SerializeField] private float fallYPanTime = 0.35f;
+    public float fallSpeedDampingChangeThreshold = -15f;
+
+    public bool isLerpingYDamping { get; private set; }
+    public bool lerpedFromPlayerFalling { get; set; }
+
+    private Coroutine lerpYPanCoroutine;
+
+    private CinemachineVirtualCamera currentCamera;
+    private CinemachineFramingTransposer framingTransposer;
+
+    private float normalPanAmount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        for (int i = 0; i < allVirtualCameras.Length; i++)
+        {
+            if (!allVirtualCameras[i].enabled) return;
+
+            currentCamera = allVirtualCameras[i];
+            framingTransposer = currentCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        }
+
+        normalPanAmount = framingTransposer.m_YDamping;
+    }
+
+    #region Lerp Y Damping
+
+    public void LerpYDamping(bool playerIsFalling)
+    {
+        lerpYPanCoroutine = StartCoroutine(LerpYAction(playerIsFalling));
+    }
+
+    private IEnumerator LerpYAction(bool playerIsFalling)
+    {
+        isLerpingYDamping = true;
+
+        // Grab starting damping amount
+        float startDampAmount = framingTransposer.m_YDamping;
+        float endDampAmount = 0f;
+
+        // Determine end damping amount
+        endDampAmount = playerIsFalling ? fallPanAmount : normalPanAmount;
+
+        if (playerIsFalling) lerpedFromPlayerFalling = true;
+
+        //Lerp pan amount
+        float elapsed = 0f;
+        while (elapsed < fallYPanTime)
+        {
+            elapsed += Time.deltaTime;
+            float lerpedPanAmount = Mathf.Lerp(startDampAmount, endDampAmount, (elapsed / fallYPanTime));
+            framingTransposer.m_YDamping = lerpedPanAmount;
+
+            yield return null;
+        }
+
+        isLerpingYDamping = false;
+
+        #endregion
+    }
+}
---
+++ Platformer/Assets/Player/Player.cs
@@ -0,0 +1,176 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Touch Areas")]
+    [SerializeField] private RectTransform leftArea;
+    [SerializeField] private RectTransform rightArea;
+    [SerializeField] private RectTransform jumpArea;
+
+    [Header("Movement")] 
+    [SerializeField] private float speed = 8;
+    [SerializeField] private float jumpingPower = 10;
+    [SerializeField] private float upwardsGravity = 2;
+    [SerializeField] private float downwardsGravity = 3.3f;
+    [SerializeField] private float groundAccelerationTime = 0.1f;
+    [SerializeField] private float airAccelerationTime = 0.2f;
+    [SerializeField] private float maxFallSpeed = 20f;
+
+    [Header("Misc")]
+    private float horizontalInput;
+    private Rigidbody2D rb;
+    public bool isFacingRight = true; // CameraFollowObject
+    private float currentVelocity;
+
+    [Header("Camera")]
+    [SerializeField] private GameObject cameraFollowGO;
+    private CameraFollowObject cameraFollowObject;
+    private float fallSpeedYDampingChangeThreshold;
+    #endregion
+
+    #region Game Loop
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Camera
+        cameraFollowObject = cameraFollowGO.GetComponent<CameraFollowObject>();
+        fallSpeedYDampingChangeThreshold = CameraManager.instance.fallSpeedDampingChangeThreshold;
+    }
+
+    private void Update()
+    {
+        TouchInput();
+        Movement();
+        CheckTurn();
+        //HandleCamera();
+    }
+
+    #endregion
+
+    #region Touch Input
+
+    private void TouchInput()
+    {
+#if UNITY_EDITOR // Else UNITY_ANDOID is buggy
+#elif UNITY_ANDROID              
+        horizontalInput = 0;
+        Debug.Log("Android");
+
+        foreach (Touch touch in Input.touches)
+        {
+            CheckTouchZones(touch.position, touch.phase);
+        }
+#endif
+    }
+
+    private void CheckTouchZones(Vector2 screenPos, UnityEngine.TouchPhase touchPhase = UnityEngine.TouchPhase.Began)
+    {
+        if (IsWithinUIArea(leftArea, screenPos))
+        {
+            horizontalInput = -1;
+        }
+        else if (IsWithinUIArea(rightArea, screenPos))
+        {
+            horizontalInput = 1;
+        }
+
+        if (IsWithinUIArea(jumpArea, screenPos) && touchPhase == UnityEngine.TouchPhase.Began)
+        {
+            Jump();
+        }
+
+        if (IsWithinUIArea(jumpArea, screenPos) && touchPhase == UnityEngine.TouchPhase.Ended)
+        {
+            CancelJump();
+        }
+    }
+
+    private bool IsWithinUIArea(RectTransform area, Vector2 screenPos)
+    {
+        if (area == null) return false;
+        return RectTransformUtility.RectangleContainsScreenPoint(area, screenPos);
+    }
+
+    #endregion
+
+    #region PlayerInput (Editor / Gamepad)
+
+#if UNITY_EDITOR
+    public void InputJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            Jump();
+
+        if (context.canceled)
+            CancelJump();
+    }
+
+    public void InputMove(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
+    }
+#endif
+
+    #endregion
+
+    #region Movement
+    private void Movement()
+    {
+        // Gravity
+        rb.gravityScale = rb.velocity.y < 0f ? downwardsGravity : upwardsGravity;
+
+        // X Vel, Y Vel Clamp
+        float targetVelocityX = horizontalInput * speed;
+        float accelerationTime = IsGrounded() ? groundAccelerationTime : airAccelerationTime;
+        float smoothedVelocityX = Mathf.SmoothDamp(rb.velocity.x, targetVelocityX, ref currentVelocity, accelerationTime);
+        rb.velocity = new Vector2(smoothedVelocityX, Mathf.Clamp(rb.velocity.y, -maxFallSpeed, maxFallSpeed));
+    }
+
+    private void Jump()
+    {
+        if (!IsGrounded()) return;
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+    }
+
+    private void CancelJump()
+    {
+        if (rb.velocity.y > 0f)
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.4f);
+    }    
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    #endregion
+
+    #region Turn
+    private void CheckTurn()
+    {
+        if (!isFacingRight && horizontalInput > 0f)
+            Turn();
+        else if (isFacingRight && horizontalInput < 0f)
+            Turn();
+    }
+
+    private void Turn()
+    {
+        Vector3 rotation = new Vector3(transform.rotation.x, isFacingRight ? 180f : 0f, transform.rotation.z);
+        transform.rotation = Quaternion.Euler(rotation);
+        cameraFollowObject.CallTurn();
+        isFacingRight = !isFacingRight;
+    }
+
+    #endregion
+
+
+}
