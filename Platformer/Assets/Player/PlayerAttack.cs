0b581b92a7ea8feeae97f718ba97f6bf15482ed1
CheckerT <tom.tb.brand@gmx.de>
2025-05-16 11:26:36 +0200

    Attack blueprint

--- Platformer/Assets/Player/PlayerAttack.cs
+++ Platformer/Assets/Player/PlayerAttack.cs
@@ -1,12 +1,74 @@
 using UnityEngine;
+using System;
 
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Player player;
-    
-    private void Attack(string attackType)
-    {
-        // Attack
-    }
+    [SerializeField] private LayerMask enemieLayer;
+    [SerializeField] private Attack[] attacks;
 
+    private float cooldown;
+    
+    private void Attack(string attackName)
+    {
+        Attack attack = attacks.where(attack => attack.name == attackName);
+        if (attack == null) return;
+        cooldown = 100f;
+        
+        Effect
+        
+        for (int i = 0; i < attack.hitRepeatAmount; i++)
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapBoxAll((Vector2)player.transform.position + attack.offset, attack.range, 0, enemieLayer);
+            foreach (Collider2D enemy in enemiesToDamage)
+            {
+                // Damage Enemie
+                
+                foreach (Effect in atack.effects)
+                {
+                    // Apply Effect
+                }
+            }
+        }
+        
+        cooldown = attack.cooldown;
+    }
+}
+
+[Serializable]
+public class Attack
+{
+    public string name;
+    public float cooldown;
+    public float castTime;
+    public float damage;
+    public int hitRepeatAmount;
+    public Vector2 range;
+    public Vector2 offset;
+    public Effect[] appliedEffects
+}
+
+[Serializable]
+public class Effect
+{
+    public EffectW effect;
+    public float duration;
+}
+
+
+public enum EffectW
+{
+    Fire,
+    Poison,
+    Bleed,
+    
+    Speed,
+    Slowness,
+    Haste,
+    AntiHaste,
+    
+    Weakness,
+    Strength,
+    AttackWeakness,
+    AttackStrength
 }
