f18afabe540a9750bce562915035f62db69a1ce0
CheckerT <tom.tb.brand@gmx.de>
2025-05-16 09:44:51 +0200

    Update Attack.cs

--- Platformer/Assets/Player/Attack.cs
+++ Platformer/Assets/Player/Attack.cs
@@ -6,5 +6,8 @@ public class PlayerAttack
     public string name
     public float cooldown;
     public float castTime;
-    public float attack;
+    public float damage;
+    public int hitRepeatAmount;
+    public Vector2 range;
+    public Vector2 offset
 }
