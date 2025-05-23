97d2f60298d548faeafd9979a3e5f198c9189b24
CheckerT <tom.tb.brand@gmx.de>
2025-05-23 11:58:53 +0200

    Create ProjectileData.cs

---
+++ Platformer/Assets/Objects/Projectile/ProjectileData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Projectiles/Projectile")]
+public class ProjectileData : ScriptableObject
+{
+    public GameObject projectilePrefab;
+    public float lifetime;
+    public int pierce;
+    public LayerMask targetMask;
+    public Vector2 initialVelocity;
+    public Vector2 gravity;
+    public Effect[] effects;
+}
