330c81ffd993d3e427900061a23407642171f670
CheckerT <tom.tb.brand@gmx.de>
2025-05-23 12:01:11 +0200

    Create ProjectileManager.cs

---
+++ Platformer/Assets/Objects/Projectile/ProjectileManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public static class ProjectileManager
+{
+    public static void Spawn(ProjectileData data, Vector2 position, Quaternion rotation)
+    {
+        if (data.projectilePrefab == null)
+        {
+            Debug.LogError("Projectile prefab is not assigned in ProjectileData!");
+            return;
+        }
+
+        GameObject instance = GameObject.Instantiate(data.projectilePrefab, position, rotation);
+        Projectile projectile = instance.GetComponent<Projectile>();
+
+        if (projectile != null)
+        {
+            projectile.SetupFromData(data);
+        }
+        else
+        {
+            Debug.LogError("Projectile prefab does not contain a Projectile component!");
+        }
+    }
+}
