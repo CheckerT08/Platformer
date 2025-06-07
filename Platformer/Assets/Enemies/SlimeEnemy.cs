65bea8299b60d0d394b534b217d0fa2b66f41c20
CheckerT <tom.tb.brand@gmx.de>
2025-06-07 14:46:17 +0200

    Interface Health Logik - 4

--- Platformer/Assets/Enemies/SlimeEnemy.cs
+++ Platformer/Assets/Enemies/SlimeEnemy.cs
@@ -2,23 +2,16 @@ using UnityEngine;
 
 public class SlimeEnemy : EnemyBase
 {
-    public override void Awake()
-    {
-        base.Awake();
-    }
-
     protected override void OnTick()
     {
-        // Bewegung
         float direction = movingRight ? 1 : -1;
         transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
 
-        // Flip bei Wand oder Klippe
         if (CheckWall() || CheckCliff())
             Flip();
     }
 
-    void Flip()
+    private void Flip()
     {
         movingRight = !movingRight;
         Vector3 scale = transform.localScale;
@@ -26,9 +19,9 @@ public class SlimeEnemy : EnemyBase
         transform.localScale = scale;
     }
 
-    public override void Die()
+    public override void OnDeath()
     {
-        base.Die();
+        base.OnDeath();
         Debug.Log("Slime explodes into goo!");
     }
 }
